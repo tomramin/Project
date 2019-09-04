8d9713c4d3a11b8432b8a5623178126b9caefb47
Tom <tramin@et.esiea.fr>
2019-09-04 17:50:52 +0200

    modification_ToDo

---
+++ src/components/ToDo.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-form v-model="valid">
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="title"
+                label="Title"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="content"
+                label="Content"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              <v-btn v-on:click="addTodo">Add</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    title: '',
+    content: '',
+    todoList: []
+  }),
+  methods: {
+    addTodo () {
+      // TODO
+      this.todoList.push({
+        title: this.title,
+        content: this.content
+      })
+      console.log(JSON.stringify(this.todoList))
+    }
+  }
+}
+</script>
