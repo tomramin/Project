5a4e4cccca3f2544983e49840d87786dbd9baa29
Tom <tramin@et.esiea.fr>
2019-09-11 09:25:48 +0200

    New Title and color

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+    <v-app-bar dark style="background-color:green" app>
+      <v-toolbar-title  class="headline text-uppercase">
+        <span>practical work 4afsi2</span></v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         text
--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-layout text-center wrap>
+    <v-layout>
       <v-form v-model="valid">
         <v-container>
           <v-row>
